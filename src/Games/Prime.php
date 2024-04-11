22295b72189571118adad1e5d69c7e5613c82334
Maxim Raychuk <raychukmaxim@gmail.com>
2024-04-11 11:44:55 +0700

    Fix Prime.php Progression.php

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -10,33 +10,34 @@ use const BrainGames\Engine\MAX_VALUE;
 
 function isPrime(int $number): bool
 {
-    if ($number <= 1) {
-        return false;
-    }
-    if ($number === 2) {
-        return true;
-    }
-    if ($number % 2 === 0) {
+    if ($number < 2) {
         return false;
     }
         $maxDivisor = sqrt($number);
-    for ($i = 3; $i <= $maxDivisor; $i += 2) {
+    for ($i = 2; $i <= $maxDivisor; $i++) {
         if ($number % $i === 0) {
             return false;
         }
     }
         return true;
 }
+
 function runPrimeGame(): void
 {
     $rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    $gameData = generateGameData();
+    runGame($gameData, $rule);
+}
+function generateGameData(): array
+{
     $gameData = [];
+
     for ($i = 0; $i < NUMBER_OF_ROUNDS; $i++) {
         $rndNum = rand(MIN_VALUE, MAX_VALUE);
         $question = "{$rndNum}";
         $correctAnswer = isPrime($rndNum) ? 'yes' : 'no';
-
         $gameData[] = [$question, $correctAnswer];
     }
-    runGame($gameData, $rule);
+
+    return $gameData;
 }
--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -12,26 +12,33 @@ const MIN_PROGRESSION_LENGTH = 5;
 function runProgressionGame(): void
 {
     $rule = "What number is missing in the progression?";
+    $gameData = generateGameData();
+    runGame($gameData, $rule);
+}
+
+function generateGameData(): array
+{
     $gameData = [];
     for ($i = 0; $i < NUMBER_OF_ROUNDS; $i++) {
-        $start = rand(MIN_VALUE, MAX_VALUE);
-        $diff = rand(MIN_VALUE, MIN_PROGRESSION_LENGTH);
-        $length = rand(MIN_PROGRESSION_LENGTH, MAX_VALUE);
-
-        $progression = [];
-        $hiddenIndex = rand(0, $length - 1);
-
-        for ($j = 0; $j < $length; $j++) {
-            if ($j === $hiddenIndex) {
-                $progression[] = '..';
-            } else {
-                $progression[] = $start + $j * $diff;
-            }
-        }
+        $progression = generateProgression();
+        $hiddenIndex = rand(0, count($progression) - 1);
+        $correctAnswer = (string)$progression[$hiddenIndex];
+        $progression[$hiddenIndex] = '..';
         $question = implode(' ', $progression);
-        $correctAnswer = (string)($start + $hiddenIndex * $diff);
-
         $gameData[] = [$question, $correctAnswer];
     }
-        runGame($gameData, $rule);
+    return $gameData;
+}
+
+function generateProgression(): array
+{
+    $start = rand(MIN_VALUE, MAX_VALUE);
+    $diff = rand(MIN_VALUE, MIN_PROGRESSION_LENGTH);
+    $length = rand(MIN_PROGRESSION_LENGTH, MAX_VALUE);
+
+    $progression = [];
+    for ($j = 0; $j < $length; $j++) {
+        $progression[] = $start + $j * $diff;
+    }
+    return $progression;
 }
